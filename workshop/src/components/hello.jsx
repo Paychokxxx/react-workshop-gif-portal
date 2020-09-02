68aff077a2f7010ec2b1b057fb8d6ff2a08e15e1
Alexey Lopato <alopato@indeed.com>
2020-09-02 19:49:10 +0900

    commit all

---
+++ workshop/src/components/hello.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Hello extends React.Component{
+    render() {
+        const { name } = this.props;
+        return <h1>{`Hello ${name}`}</h1>;
+    }
+}
+
+export default Hello;
--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,14 @@
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from "./components/hello"
+
+// const Hello = (props) => {
+//   const { name } = props;
+//   return <h1>{`Hello ${name}`}</h1>;
+// };
+
+const root = document.querySelector("#root");
+ReactDOM.render(<Hello name="Bob" />, root); // render component inside root
